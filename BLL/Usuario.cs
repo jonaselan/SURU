9198690edaa409981db4bd6df29e221b29dbd000
Igor Fernandes <igorrfc10@gmail.com>
2015-08-19 20:49:04 -0300

    Criando classe BLL/Usuario

---
+++ BLL/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    class Usuario
+    {
+        protected int idUsuario;
+        protected string nome;
+        protected string matricula;
+        protected string telefone;
+    }
+}
