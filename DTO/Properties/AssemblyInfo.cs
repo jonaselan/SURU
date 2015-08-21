ed246c188fd3e7019e5c2e3d01c642febd5d3dad
Jonathan Batista de Oliveira Silva <jonathan.b.d.o.s@gmail.com>
2015-08-21 18:03:32 -0300

    Reorganização dos Projetos; Criada camada DTO e o modelo de usuário.

--- DTO/Properties/AssemblyInfo.cs
+++ DTO/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SURU")]
+[assembly: AssemblyTitle("DTO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SURU")]
+[assembly: AssemblyProduct("DTO")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52a24a02-0fe9-45bc-b6c1-411dd0884aa7")]
+[assembly: Guid("a1ac1256-9d85-4dd1-b4a4-cefe3f82e5d1")]
 
 // Version information for an assembly consists of the following four values:
 //
