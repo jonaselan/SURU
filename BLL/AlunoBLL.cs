945bd63f8bfda9c2bf1df59c552adc8e1cc53ade
Igor Fernandes <igorrfc10@gmail.com>
2015-08-23 10:15:07 -0300

    Criada a classe BLL/AlunoBLL,a interface BLL/IAluno, modelada a classe DTO/Aluno

---
+++ BLL/AlunoBLL.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using DAL;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Security.Cryptography;
+
+namespace BLL
+{
+    public class AlunoBLL : IAluno, IDBElement<Aluno>
+    {
+
+        public Usuario ConsultarPorMatricula(string matricula)
+        {
+            DBConnect db = new DBConnect(0);
+            return db.Select<Usuario>().FirstOrDefault(u => u.Matricula == matricula);
+        }
+
+        public List<Aluno> Listar()
+        {
+            DBConnect db = new DBConnect(1);
+            return db.Select<Aluno>();
+        }
+
+        public void Inserir(Aluno e)
+        {
+            throw new Exception("Perfil não especificado.");
+        }
+
+        public void Alterar(Aluno e)
+        {
+            /* RETIRANDO OS ESPAÇOS */
+            DBElementHandling.RemoverEspacos(e);
+
+            /* CHECAGEM DOS VALORES INSERIDOS */
+            try { IsValido(e); }
+            catch (Exception ex) { throw ex; }
+
+
+            /* CONECTANDO AO DB */
+            DBConnect db = new DBConnect(1);
+            db.Update(e);
+        }
+
+        void IDBElement<Aluno>.Remover(Aluno e)
+        {
+            /* RETIRANDO OS ESPAÇOS */
+
+            DBElementHandling.RemoverEspacos(e);
+
+            DBConnect db = new DBConnect(1);
+            db.Delete(e);
+        }
+
+        Aluno IDBElement<Aluno>.ConsultarPorId(int id)
+        {
+            DBConnect db = new DBConnect(1);
+            return db.Select<Aluno>().FirstOrDefault(p => p.Id == id);
+        }
+
+        public void IsValido(Aluno e)
+        {
+            if (e.Nome == "") { throw new Exception("wewewoewoeow"); } // Adicionar erro
+            if (e.Curso == "") { throw new Exception("curso sem delicia"); }
+        }
+
+        Aluno IAluno.ConsultarPorNome(string nome)
+        {
+            DBConnect db = new DBConnect(1);
+            return db.Select<Aluno>().FirstOrDefault(p => p.Nome == nome);
+        }
+    }
+}
--- DTO/Aluno.cs
+++ DTO/Aluno.cs
@@ -7,8 +7,81 @@ using System.Threading.Tasks;
 namespace DTO
 {
     /* Modelo de Aluno, aqui ficara as informações bolsista, diaria, etc. */
-    class Aluno : Perfil
+    public class Aluno : Perfil
     {
-        /* Falta modelar */
+        private bool isBolsista;
+        private bool segunda;
+        private bool terca;
+        private bool quarta;
+        private bool quinta;
+        private bool sexta;
+        private string curso;
+        private string turno;
+        private string semestre;
+
+        public bool IsBolsista
+        {
+            set { this.isBolsista = value; }
+            get { return this.isBolsista; }
+
+        }
+
+        public bool Segunda 
+        {
+
+            set { this.segunda = value; }
+            get { return this.segunda; }
+        
+        }
+
+        public bool Terca
+        {
+
+            set { this.terca = value; }
+            get { return this.terca; }
+
+        }
+
+        public bool Quarta
+        {
+
+            set { this.quarta = value; }
+            get { return this.quarta; }
+
+        }
+
+        public bool Quinta
+        {
+
+            set { this.quinta = value; }
+            get { return this.quinta; }
+
+        }
+
+        public bool Sexta
+        {
+
+            set { this.sexta = value; }
+            get { return this.sexta; }
+
+        }
+
+        public string Curso 
+        {
+            set { this.curso = value; }
+            get { return this.curso; }
+        }
+
+        public string Turno 
+        {
+            set { this.turno = value; }
+            get { return this.turno; }
+        }
+
+        public string Semestre 
+        {
+            set { this.semestre = value; }
+            get { return this.semestre; }
+        }
     }
 }
